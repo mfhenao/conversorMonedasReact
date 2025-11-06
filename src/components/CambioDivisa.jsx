cc57a95b3863d8000f419b4cb8fb8088939aaf01
Camilo Hernandez <camilohi.1196@gmail.com>
2025-11-06 12:01:17 -0500

    Creacion de componente
    Cambio Divisa

---
+++ src/components/CambioDivisa.jsx
@@ -0,0 +1,15 @@
+// src/components/CambioDivisa.jsx
+
+import React, { useState } from 'react';
+
+const CambioDivisa = () => {
+
+  const API_KEY = '71653b5ed6a7daa58940ce22';
+
+  // return (
+
+  // );
+  
+};
+
+export default CambioDivisa;
